430b28c6dc18b8588e82765a519b831a50d4b5cf
reynar <ars_k00@mail.ru>
2023-02-08 17:17:13 +0300

    Spring MVC setup with controllers and parameters

--- FirstWebApp/src/main/java/com/fox/example/controllers/FirstController.java
+++ FirstWebApp/src/main/java/com/fox/example/controllers/FirstController.java
@@ -1,15 +1,19 @@
 package com.fox.example.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/game")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname, Model model) {
+//        System.out.println(name + " " + surname);
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
